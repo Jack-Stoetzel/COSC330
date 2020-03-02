aa96c41b8300901fd859f50d872666af002271c8
Jack Stoetzel <stoetzeljt@gmail.com>
2020-03-01 20:39:29 -0500

    committing final version

--- Lab2/Cookie/app/src/main/java/com/example/cookie/MainActivity.java
+++ Lab2/Cookie/app/src/main/java/com/example/cookie/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.cookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void feed(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        image.setVisibility(View.VISIBLE);
+        Button button1 = (Button) findViewById(R.id.FeedingButton);
+        button1.setText(R.string.done);
+
     }
 }

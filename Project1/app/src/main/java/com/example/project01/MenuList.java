bace2c6549fe74aac6d567110073e323ea7d4514
Jack Stoetzel <stoetzeljt@gmail.com>
2020-04-08 14:06:14 -0400

    Renamed files

---
+++ Project1/app/src/main/java/com/example/project01/MenuList.java
@@ -0,0 +1,74 @@
+package com.example.project01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MenuList extends AppCompatActivity {
+
+    private ListView menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_list);
+
+        menu = findViewById(R.id.menu);
+
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+
+        final String[] items = getResources().getStringArray(R.array.menu);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+
+        menu.setAdapter(adapter);
+
+        super.onResume();
+
+        menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent;
+                switch(i)
+                {
+                    case 0:
+                        intent = new Intent(getApplicationContext(),WordList.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(getApplicationContext(), CrosswordActivity.class);
+                        startActivity(intent);
+                        break;
+//                    case 2:
+//                        intent = new Intent(getApplicationContext(),.class);
+//                        startActivity(intent);
+//                        break;
+//                    case 3:
+//                        Intent intent = new Intent(getApplicationContext(),.class);
+//                        startActivity(intent);
+//                        break;
+//                    case 4:
+//                        Intent intent = new Intent(getApplicationContext(),.class);
+//                        startActivity(intent);
+//                        break;
+//                    case 5:
+//                        Intent intent = new Intent(getApplicationContext(),.class);
+//                        startActivity(intent);
+//                        break;
+                }
+                //intent.putExtra("name",items[i]);
+            }
+        });
+    }
+}
